c1e63bdf69fe172d22e145f46677858238a78192
Jason Walsh <jwalsh1@chewy.com>
2020-07-17 11:42:55 -0400

    Pull recording

---
+++ get-recording.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+mkdir -p data 
+wget -O data/radio-transmission-recording.wav https://message-from-space.readthedocs.io/en/latest/_downloads/822b494d13eb35a97f159e041a004fa9/radio-transmission-recording.wav
