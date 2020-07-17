1e914259c2411c30528afcc355ba7c5a01ae8c16
Jason Walsh <j@wal.sh>
2020-07-17 19:13:14 -0400

    Pull full message

--- get-message.sh
+++ get-message.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-mkdir -p data 
+mkdir -p data
 wget -O data/radio-transmission-recording.wav https://message-from-space.readthedocs.io/en/latest/_downloads/822b494d13eb35a97f159e041a004fa9/radio-transmission-recording.wav
+
+
+wget -O data/galaxy.txt https://message-from-space.readthedocs.io/en/latest/_downloads/48286a0b4dac94efc1b23fb2c3a41bef/galaxy.txt
